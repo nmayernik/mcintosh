10dd38de497bb68f2f796bab974756b70c50b612
Nick Mayernik <nickmayernik@me.com>
2021-09-16 13:18:53 -0400

    Update

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -2,6 +2,38 @@ import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Link from 'next/link';
 
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'caseStudy'
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+      content_type: 'caseStudy',
+      'fields.slug': params.slug
+  })
+
+  return {
+    props: { caseStudy: items[0] }
+  }
+}
 
 
 export default function Slug({ caseStudy }) {
@@ -53,35 +85,3 @@ export default function Slug({ caseStudy }) {
     </section>
 )};
 
-const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
-  
-  export const getStaticPaths = async () => {
-    const res = await client.getEntries({
-      content_type: 'caseStudy'
-    })
-  
-    const paths = res.items.map(item => {
-      return {
-        params: { slug: item.fields.slug }
-      }
-    })
-  
-    return {
-      paths,
-      fallback: true
-    }
-  }
-  
-  export async function getStaticProps({ params }) {
-    const { items } = await client.getEntries({
-        content_type: 'caseStudy',
-        'fields.slug': params.slug
-    })
-  
-    return {
-      props: { caseStudy: items[0] }
-    }
-  }
